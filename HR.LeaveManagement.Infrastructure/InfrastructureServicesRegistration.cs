59a58202cd5e36430633c37048ed1575b77a26a6
daniyalaslam54 <daniyalaslam54@gmail.com>
2024-01-26 23:59:40 +0500

    addded email service

---
+++ HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,25 @@
+﻿using HR.LeaveManagement.Application.Email;
+using HR.LeaveManagement.Application.Logging;
+using HR.LeaveManagement.Application.Models.Email;
+using HR.LeaveManagement.Infrastructure.EmailService;
+using HR.LeaveManagement.Infrastructure.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Infrastructure;
+
+public static class InfrastructureServicesRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<EmailSettings>(options =>
+        {
+            options.ApiKey = configuration["EmailSettings:ApiKey"];
+            options.FromAddress = configuration["EmailSettings:FromAddress"];
+            options.FromName = configuration["EmailSettings:FromName"];
+        });
+        services.AddTransient<IEmailSender, EmailSender>();
+        services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
+        return services;
+    }
+}
